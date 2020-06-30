9376d54b59256cb5e17d1d73a0e56db43172529a
NicollasDileo06 <nicgato2013@gmail.com>
2020-06-30 15:48:30 -0300

    Atividade concluida

--- Produto.cs
+++ Produto.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Aula27_28_29_30
@@ -7,7 +9,7 @@ namespace Aula27_28_29_30
         public int Codigo { get; set; }
         public string Nome { get; set; }
         public float Preco { get; set; }
-        private const string  PATH = "Datebase/Produto.csv";
+        private const string PATH = "Datebase/Produto.csv";
 
         public Produto()
         {
@@ -18,12 +20,57 @@ namespace Aula27_28_29_30
           }
         }
 
+    /// <summary>
+    /// Cadastra um produto
+    /// </summary>
+    /// <param name="prod">Objeto</param>
+
          public void Cadastrar(Produto prod)
          {
             var linha = new string[] { PrepararLinha(prod) };
             File.AppendAllLines(PATH, linha);
          }
-         
+
+         /// <summary>
+         /// Lê o csv 
+         /// </summary>
+         /// <returns>Lista de produtos</returns>
+
+         public List<Produto> Ler()
+         {
+             //criamos uma lista que servira como nosso retorno
+             List<Produto> produtos = new List<Produto>();
+
+             // Lemos o arquivo e transformamos em um array de linhas 
+             // [0] = codigo=1;nome=Gibson;preco=4500
+             // [1] = codigo=1;nome=Fender;preco=4500
+             string[] linhas = File.ReadAllLines(PATH);
+
+             foreach(string linha in linhas ){
+                 
+                 // Separamos os dados de cada linha com Split
+                 // [0] = codigo=1
+                 // [1] = nome=Gibson
+                 // [2] = preco=7500
+                string[] dado = linha.Split(";");
+
+                 // Criamos instâncias de produtos para serem colocados na lista
+                 Produto p = new Produto();
+                 p.Codigo = Int32.Parse( Separar(dado[0] ));
+                 p.Nome = Separar(dado[1]);
+                 p.Preco = float.Parse( Separar(dado[2]));
+
+                produtos.Add(p);
+             }
+
+             return produtos;
+         } 
+         private string Separar(string _coluna)
+         {
+             //0      1
+             //nome =  Gibson
+             return _coluna.Split("=")[1];
+         }
 
        // 1;Celular;600
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Aula27_28_29_30
 {
@@ -9,10 +10,26 @@ namespace Aula27_28_29_30
             
             Produto p1 = new Produto();
             p1.Codigo = 1;
-            p1.Nome = "Gibson";
-            p1.Preco = 4500f;
+            p1.Nome = "Ela";
+            p1.Preco = 99f;
 
             p1.Cadastrar(p1);
+
+            List<Produto> lista = p1.Ler();
+          
+           foreach(Produto item in lista){
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                System.Console.WriteLine($"R$:{item.Preco} - {item.Nome}");
+            }
+
+            //Buscar por Nome através da expressão lambda
+            Produto R6 = lista.Find(x => x.Nome == "Ela");
+
+            System.Console.WriteLine(R6.Nome);
+
         }
+        
+        
     }
 }
+
